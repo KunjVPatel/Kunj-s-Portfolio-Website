202064d860b1a70eea66c38da544b18567a2fa30
Kunj <KunjVPatel321@gmail.com>
2024-06-28 07:31:56 -0700

    Further testing of theme switching

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,14 @@ import vercelStatic from '@astrojs/vercel/static';
 import react from '@astrojs/react';
 import sitemap from "@astrojs/sitemap";
 
+import fs from 'fs';
 import vercel from '@astrojs/vercel/serverless';
 
+// Determine theme based on environment variable
+const theme = process.env.THEME === 'light' ? './src/themes/gruvbox-light.json' : './src/themes/gruvbox-dark.json';
+
 const options = {
-  //theme: json,
-  theme: JSON.parse(fs.readFileSync("./src/themes/gruvbox-dark.json", "utf-8")),
+  theme: JSON.parse(fs.readFileSync(theme, "utf-8")), // Use the determined theme
   onVisitLine(node) {
     if (node.children.length === 0) {
       node.children = [{
@@ -25,8 +28,7 @@ const options = {
   }
 };
 
-
-// https://astro.build/config
+// Astro configuration
 export default defineConfig({
     site: 'https://kunj-s-portfolio-website.vercel.app',
     markdown: {
